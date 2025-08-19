2b0dead85beebe5a708bb1af618202ee32e814c8
nishu-murmu <nishumurmu017@gmail.com>
2025-08-20 00:40:13 +0530

    chore: build errors.

--- apps/web/src/pages/Admin/AR/ManageMembers.tsx
+++ apps/web/src/pages/Admin/AR/ManageMembers.tsx
@@ -1,6 +1,8 @@
 import AllUsers from '../Users/AllUsers';
 
 const ManageMembers = () => {
+  // NOTE: Task for Mithlesh to complete, please show the table
+  // Please use ag-grid table.
   return <AllUsers />;
 };
 
