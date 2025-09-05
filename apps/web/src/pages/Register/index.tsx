1d16c1e97fa3dba7e71daced391b74460c801afc
nishu-murmu <71123337+nishu-murmu@users.noreply.github.com>
2025-09-05 17:41:27 +0530

    feat: Introduce ClientLayout and refactor routing
    - Added a new ClientLayout component to manage user navigation and layout structure.
    - Removed the obsolete RootLayout component to streamline the layout management.
    - Updated routing to utilize ClientLayout for better user experience and authentication handling.
    - Introduced ProtectedRoute and AdminRoute components for enhanced route protection based on user authentication status.

--- apps/web/src/pages/Register/index.tsx
+++ apps/web/src/pages/Register/index.tsx
@@ -1,21 +1,19 @@
-import { Navigate } from "react-router-dom";
-import SignUpForm from "./SignupForm";
-import { useAuth } from "@/contexts/AuthContext";
 import { useState } from "react";
 import { SignUpCard } from "./SignUpCard";
+import SignUpForm from "./SignupForm";
 
 export default function Register() {
-  const { user } = useAuth();
   const [isSignUpOpen, setIsSignUpOpen] = useState(false);
-
-  if (user) {
-    return <Navigate to="/" replace />;
-  }
 
   return (
     <div className="bg-gradient-to-br from-primary/20 to-secondary/20 min-h-screen flex items-center justify-center">
       <SignUpCard onSignUpClick={() => setIsSignUpOpen(true)} />
-      {isSignUpOpen && <SignUpForm isOpen={isSignUpOpen} onClose={() => setIsSignUpOpen(false)} />}
+      {isSignUpOpen && (
+        <SignUpForm
+          isOpen={isSignUpOpen}
+          onClose={() => setIsSignUpOpen(false)}
+        />
+      )}
     </div>
   );
 }
