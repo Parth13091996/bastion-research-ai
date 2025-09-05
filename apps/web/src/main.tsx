06264cb96454bc139ae22eabc63a1da601a45f3d
nishu-murmu <71123337+nishu-murmu@users.noreply.github.com>
2025-09-05 13:47:28 +0530

    feat: adding page-loader in react application.

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { AuthProvider } from "./contexts/AuthContext";
+import { LoaderProvider } from "./contexts/LoaderContext";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <App />
+        <LoaderProvider>
+          <App />
+        </LoaderProvider>
       </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
